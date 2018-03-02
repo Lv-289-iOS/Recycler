588e0ca4daca4bb1153ac548bacd8d8dc1524a1d
Artem Reznikov <temarez@gmail.com>
2018-03-03 00:06:08 +0200

    Initial implementation of QR code reading from camera

--- Recycler/Controllers/RCLScanVC.swift
+++ Recycler/Controllers/RCLScanVC.swift
@@ -7,29 +7,94 @@
 //
 
 import UIKit
+import AVFoundation
 
-class RCLScanVC: UIViewController {
+protocol BarcodeDelegate: class {
+    func barcodeRead(barcode: String)
+}
 
+class RCLScanVC: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    weak var delegate: BarcodeDelegate?
+    
+    var output = AVCaptureMetadataOutput()
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer!
+    
+    var captureSession = AVCaptureSession()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        setupCamera()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if !captureSession.isRunning {
+            captureSession.startRunning()
+        }
     }
-    */
-
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if captureSession.isRunning {
+            captureSession.stopRunning()
+        }
+    }
+    
+    private func setupCamera() {
+        guard let device = AVCaptureDevice.default(for: .video),
+            let input = try? AVCaptureDeviceInput(device: device) else {
+                return
+        }
+        
+        if captureSession.canAddInput(input) {
+            captureSession.addInput(input)
+        }
+        
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer.videoGravity = .resizeAspectFill
+        videoPreviewLayer.frame = view.bounds
+        view.layer.addSublayer(videoPreviewLayer)
+        
+        let metadataOutput = AVCaptureMetadataOutput()
+        
+        if captureSession.canAddOutput(metadataOutput) {
+            captureSession.addOutput(metadataOutput)
+            
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [.qr] // metadataOutput.availableMetadataObjectTypes
+            // TODO: metadataOutput.rectOfInterest
+        } else {
+            print("Could not add metadata output")
+        }
+    }
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        // This is the delegate's method that is called when a code is read
+        for metadata in metadataObjects {
+            if let readableObject = metadata as? AVMetadataMachineReadableCodeObject,
+                let code = readableObject.stringValue {
+                dismiss(animated: true)
+                delegate?.barcodeRead(barcode: code)
+                print(code)
+                
+                captureSession.stopRunning()
+                let alert = UIAlertController(title: nil, message: code, preferredStyle: .alert)
+                self.present(alert, animated: true)
+                
+                // duration in seconds
+                let duration: Double = 5
+                
+                DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + duration) {
+                    alert.dismiss(animated: true)
+                    self.captureSession.startRunning()
+                }
+                
+            }
+        }
+    }
+    
 }
