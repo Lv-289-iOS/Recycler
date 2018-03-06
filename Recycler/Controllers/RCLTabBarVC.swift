8eefbee19c0e542dcfd74183ebd52fddaae072a3
Artem Reznikov <temarez@gmail.com>
2018-03-06 14:24:54 +0200

    Added RCLTabBarVC class

---
+++ Recycler/Controllers/RCLTabBarVC.swift
@@ -0,0 +1,35 @@
+//
+//  RCLTabBarVC.swift
+//  Recycler
+//
+//  Created by Artem Rieznikov on 06.03.18.
+//  Copyright © 2018 softserve.university. All rights reserved.
+//
+
+import UIKit
+
+class RCLTabBarVC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
