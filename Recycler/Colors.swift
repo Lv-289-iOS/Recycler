df62fdac5c2bae2eda0f09443203a2a4fd7f030b
Artem Reznikov <temarez@gmail.com>
2018-03-12 12:41:06 +0200

    Improve scan ViewController

--- Recycler/Colors.swift
+++ Recycler/Colors.swift
@@ -10,6 +10,8 @@ import Foundation
 import UIKit
 
 extension UIColor {
+    static var darkModeratePink: UIColor { return UIColor(red:0.60, green:0.20, blue:0.40, alpha:1.0) }
+    
     struct Backgrounds {
         static var GrayLight: UIColor  { return UIColor(red:0.15, green:0.17, blue:0.18, alpha:1.0) }
         static var GrayDark: UIColor  { return UIColor(red:0.08, green:0.09, blue:0.11, alpha:1.0) }
@@ -19,7 +21,7 @@ extension UIColor {
         static var Gray: UIColor { return UIColor(red:0.39, green:0.40, blue:0.41, alpha:1.0) }
     }
     struct TabBar {
-        static var tint: UIColor { return UIColor(red:0.60, green:0.20, blue:0.40, alpha:1.0) }
+        static var tint: UIColor { return darkModeratePink }
         static var barTint: UIColor { return UIColor(red:0.00, green:0.00, blue:0.00, alpha:1.0) }
     }
 }
