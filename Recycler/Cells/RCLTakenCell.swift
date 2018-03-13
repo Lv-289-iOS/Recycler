67d03cb4cd91b4cb282ec595a7d5734236d599d6
danylonovosad <danylo.novosad@gmail.com>
2018-03-13 17:58:41 +0200

    no message

--- Recycler/Cells/RCLTakenCell.swift
+++ Recycler/Cells/RCLTakenCell.swift
@@ -9,16 +9,51 @@
 import UIKit
 
 class RCLTakenCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+   
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var size: UILabel!
+    @IBOutlet weak var phone: UILabel!
+    
+    @IBOutlet weak var location: UILabel!
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var viewContainer: UIView!
+    
+      var trashCan = TrashCan()
+    
+    func configureCell(forCan: TrashCan) {
+        colorsSetup()
+       self.trashCan = forCan
+        switch trashCan.isFull {
+        case true:
+            viewContainer.backgroundColor = UIColor.Backgrounds.GrayLighter
+            self.isUserInteractionEnabled = true
+        case false:
+            viewContainer.backgroundColor = UIColor.Backgrounds.GrayLight
+            isUserInteractionEnabled = false
+        }
+        switch trashCan.type {
+        case "plastic" :
+            self.icon.image = #imageLiteral(resourceName: "trash_plastic")
+        case "metal" :
+            self.icon.image = #imageLiteral(resourceName: "trash_metal")
+        case "organic" :
+            self.icon.image = #imageLiteral(resourceName: "trash_organic")
+        case "battaries" :
+            self.icon.image = #imageLiteral(resourceName: "battery")
+        default:
+            self.icon.image = #imageLiteral(resourceName: "trash_other")
+        }
+        self.name.text = forCan.type
+        self.location.text = forCan.address
+        //        self.size.text = forCan.size
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func colorsSetup() {
+        name.textColor = UIColor.Font.White
+        location.textColor = UIColor.Font.Gray
+        size.textColor = UIColor.Font.White
+        viewContainer.layer.cornerRadius = CGFloat.Design.CornerRadius
+        
+        backgroundColor = UIColor.Backgrounds.GrayDark
     }
     
 }
