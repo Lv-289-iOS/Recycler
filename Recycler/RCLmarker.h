3979c240d99fb365f0a47e925a872d8c04b36e57
Yurii Tsymbala <>
2018-03-20 11:00:48 +0200

    Added new pod

---
+++ Recycler/RCLmarker.h
@@ -0,0 +1,14 @@
+//
+//  RCLmarker.h
+//  Recycler
+//
+//  Created by Yurii Tsymbala on 20.03.18.
+//  Copyright © 2018 softserve.university. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "GMUMarkerClustering.h"
+
+@protocol RCLmarker <NSObject>
+
+@end
