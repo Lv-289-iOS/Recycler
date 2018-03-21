d66f2a06d446c4f0648f7a76b212492cacd0c90d
Yurii Tsymbala <zumbala2013@gmail.com>
2018-03-21 17:34:12 +0200

    Refactored maps

--- Recycler/Controllers/RCLMapVC.swift
+++ Recycler/Controllers/RCLMapVC.swift
@@ -11,21 +11,40 @@ import GoogleMaps
 import CoreLocation
 
 class RCLMapVC: UIViewController {
-
-    @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var mapView: GMSMapView!
-
-
     
+    @IBOutlet weak var mapView: GMSMapView!
+    
+    private var locationManager = CLLocationManager()
+    private var userLocation = CLLocation()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let camera = GMSCameraPosition.camera(withLatitude: 49.841608, longitude: 24.031728, zoom: 16.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        addTitleLabel(text: "Public trash cans")
+        addMarkers()
+        customizeMap()
+        loadMap()
+    }
+    
+    private func loadMap() {
         mapView.isMyLocationEnabled = true
         mapView.settings.myLocationButton = true
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = 50
+        if let locationOfUser = locationManager.location {
+            userLocation = locationOfUser
+            animateCameraTo(coordinate: userLocation.coordinate)
+        }
+    }
+    
+    private func animateCameraTo(coordinate: CLLocationCoordinate2D, zoom: Float = 16.0) {
+        let camera = GMSCameraPosition.camera(withTarget: coordinate, zoom: zoom)
+        mapView.animate(to: camera)
+    }
+    
+    private func customizeMap() {
         do {
-            // Set the map style by passing the URL of the local file.
             if let styleURL = Bundle.main.url(forResource: "RCLMapStyle", withExtension: "json")
             {
                 mapView.mapStyle = try GMSMapStyle(contentsOfFileURL: styleURL)
@@ -35,38 +54,23 @@ class RCLMapVC: UIViewController {
         } catch {
             NSLog("One or more of the map styles failed to load. \(error)")
         }
-        view = mapView
-    
-        addMarkers()
-        addTitleLabel(text: "Public trash cans")
     }
-  
-
     
     private func addMarkers(){
         let parsed = RLCParsingByJSON()
         parsed.temp { (trashList, error) in
-            
-            let marker = GMSMarker()
-            let pos = CLLocationCoordinate2D(latitude: 49.839734, longitude: 24.036009)
-            marker.position = pos
-            marker.map = self.view as? GMSMapView
-//            if error == nil {
-//                for trash in trashList! {
-//                    let markerImage = #imageLiteral(resourceName: "smallPin")
-//                    let marker = GMSMarker()
-//                    marker.position = trash.coordinate
-//                    marker.title = trash.nameInJson
-//                    marker.snippet = "Кількість смітників: \(trash.numberOfRaffleInJson)"
-//                    marker.icon = markerImage//GMSMarker.markerImage(with: .red)
-//                    marker.map = self.view as? GMSMapView
-//
-//                }
-//            }
-            
+            if error == nil {
+                for trash in trashList! {
+                    let markerImage = #imageLiteral(resourceName: "smallPin")
+                    let marker = GMSMarker()
+                    marker.position = trash.coordinate
+                    marker.title = trash.nameInJson
+                    marker.snippet = "Кількість смітників: \(trash.numberOfRaffleInJson)"
+                    marker.icon = markerImage
+                    marker.map = self.mapView
+                }
+            }
             print("TrashCount \(String(describing: trashList?.count)) error: \(String(describing: error))")
-}
+        }
     }
-
-    
 }
